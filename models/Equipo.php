1d2f8bad48f940bf62d316c022df26f3f02a5f95
alexcotte <alexanortega@gmail.com>
2016-08-31 23:09:57 -0600

    Planificacion

---
+++ models/Equipo.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "equipo".
+ *
+ * @property integer $id_equipo
+ * @property string $descripcion
+ * @property string $estado
+ * @property string $fecha_creacion
+ * @property integer $id_plan
+ *
+ * @property AutomorEquipo[] $automorEquipos
+ * @property Automotor[] $automors
+ * @property Plan $plan
+ * @property OrdenTrabajo[] $ordenTrabajos
+ * @property Personal[] $personals
+ * @property Empleado[] $empleados
+ */
+class Equipo extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'equipo';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['descripcion', 'id_plan'], 'required'],
+            [['estado'], 'string'],
+            [['fecha_creacion'], 'safe'],
+            [['id_plan'], 'integer'],
+            [['descripcion'], 'string', 'max' => 150],
+            [['id_plan'], 'exist', 'skipOnError' => true, 'targetClass' => Plan::className(), 'targetAttribute' => ['id_plan' => 'id_plan']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_equipo' => 'Id Equipo',
+            'descripcion' => 'Descripcion',
+            'estado' => 'Estado',
+            'fecha_creacion' => 'Fecha Creacion',
+            'id_plan' => 'Id Plan',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAutomorEquipos()
+    {
+        return $this->hasMany(AutomorEquipo::className(), ['id_equipo' => 'id_equipo']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAutomors()
+    {
+        return $this->hasMany(Automotor::className(), ['id_automotor' => 'id_automor'])->viaTable('automor_equipo', ['id_equipo' => 'id_equipo']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPlan()
+    {
+        return $this->hasOne(Plan::className(), ['id_plan' => 'id_plan']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrdenTrabajos()
+    {
+        return $this->hasMany(OrdenTrabajo::className(), ['id_equipo' => 'id_equipo']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPersonals()
+    {
+        return $this->hasMany(Personal::className(), ['id_equipo' => 'id_equipo']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmpleados()
+    {
+        return $this->hasMany(Empleado::className(), ['id_empleado' => 'id_empleado'])->viaTable('personal', ['id_equipo' => 'id_equipo']);
+    }
+}
