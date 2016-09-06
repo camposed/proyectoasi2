e594d211d9af726afd105d160bbee4f212b40b43
alexcotte <alexanortega@gmail.com>
2016-09-05 21:54:48 -0600

    cambio para usar español

---
+++ messages/es/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 5/9/16
+ * Time: 9:39 PM
+ */
+
+return [
+    'Showing' => 'Mostrando',
+    'item' => 'item',
+    'items' => 'items',
+    'of' => 'de',
+    'create'=>'crear'
+];
