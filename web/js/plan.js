63de681473737cb9120308a4471e01d536419f85
alexcotte <alexanortega@gmail.com>
2016-09-07 16:22:50 -0600

    Planificacion

--- web/js/plan.js
+++ web/js/plan.js
@@ -2,5 +2,34 @@
  * Created by alex on 7/9/16.
  */
 $(window).on('shown.bs.modal', function() {
-    $('.modal-backdrop').removeClass("modal-backdrop");
+	trickModal();
 });
+function edit(key){
+	
+	
+	
+	$('#updateContent').load('render-form?id='+key);
+	
+	 $("#modaledit").modal();
+	   trickModal();
+	
+	
+	/*
+	$.ajax({
+	    type     :'GET',
+	    url  	 : 'load',
+	    data:{
+	    		id:key 
+	    },
+	    success  : function(data) {
+	       $("body").append(data.id_plan);
+	   },
+	   complete:function (data){
+		  
+	   }
+	});
+	*/
+}
+function trickModal(){
+	$('.modal-backdrop').removeClass("modal-backdrop");
+}
